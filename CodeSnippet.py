a68cf40a169ddbc5968e196a42ba9b2e55a59d04
fazalkhan14 <66491636+fazalkhan14@users.noreply.github.com>
2020-09-09 23:29:26 +0530

    Created code snippets
    This file assigns elements to the lists, accesses items from the tuple, and deletes items from the dictionary.

---
+++ CodeSnippet.py
@@ -0,0 +1,20 @@
+#assinging elements to lists
+list1 = [1,2,3]
+list2 = ["apple","mango","banana"]
+print(list1)
+print(list2)
+#accessing items from the tuple
+MyTuple = ("first","second","third","fourth")
+print(MyTuple[0])
+print(MyTuple[1])
+print(MyTuple[2])
+print(MyTuple[3])
+#deleting elements from the dictionary
+MyDict = {
+    "student1":"John",
+    "student2":"Smith",
+    "student3":"Jacob",
+    "student4":"Tony",
+    }
+del MyDict["student3"]
+print(MyDict)
